d28082d9e3c3ddbef2b9db4482b8ccb70a973ba3
Nika <tsogiaidze@yahoo.com>
2017-11-17 07:36:34 +0000

    Added dinamyc responsiveness to screen resize

--- chart.js
+++ chart.js
@@ -1,21 +1,27 @@
 var margin = { top:0 , right:0 , bottom:20 , left:30 }
 
-var screen_width = get_screen_width_height().width;
-var screen_height = get_screen_width_height().height;
+var screen_width;
+var screen_height;
 
-var chart_container_width = define_chart_container_width(screen_width);
-var chart_container_height = define_chart_container_height(screen_height);
+var chart_container_width;
+var chart_container_height;
 
-var chart_width = chart_container_width - margin.right - margin.left;
-var chart_height = chart_container_height - margin.top - margin.bottom;
+var chart_width;
+var chart_height;
 
 var months = [{"Month":"Jan", "Quantity": 0}, {"Month":"Feb","Quantity": 0}, {"Month":"Mar","Quantity": 0}, {"Month":"Apr","Quantity": 0}, {"Month":"May","Quantity": 0}, {"Month":"Jun","Quantity": 0}, {"Month":"Jul","Quantity": 0}, {"Month":"Aug","Quantity": 0}, {"Month":"Sep","Quantity": 0}, {"Month":"Oct","Quantity": 0}, {"Month":"Nov","Quantity": 0}, {"Month":"Dec","Quantity": 0}];
 
 var month_details = [ { "Day" : "1", "Quantity" :22 }, { "Day" : "2", "Quantity" :16 }, {"Day" : "3", "Quantity" :42}, {"Day" : "4", "Quantity" :19}, {"Day" : "5", "Quantity" :38}, {"Day" : "6", "Quantity" :46}, { "Day" : "7", "Quantity" :25 }, { "Day" : "8", "Quantity" :33 }, { "Day" : "9", "Quantity" :14 }, { "Day" : "10", "Quantity" :16 }, { "Day" : "11", "Quantity" :41 }, { "Day" : "12", "Quantity" :18 }, { "Day" : "13", "Quantity" :8 }, { "Day" : "14", "Quantity" :35 }, { "Day" : "15", "Quantity" :22 }, { "Day" : "16", "Quantity" :29 }, { "Day" : "17", "Quantity" :13 }, { "Day" : "18", "Quantity" :17 }, { "Day" : "19", "Quantity" :46 }, { "Day" : "20", "Quantity" :40 }, { "Day" : "21", "Quantity" :15 }, { "Day" : "22", "Quantity" :27 }, { "Day" : "23", "Quantity" :19 }, { "Day" : "24", "Quantity" :22 }, { "Day" : "25", "Quantity" :29 }, { "Day" : "26", "Quantity" :37 }, { "Day" : "27", "Quantity" :34 }, { "Day" : "28", "Quantity" :38 }, { "Day" : "29", "Quantity" :15 }, { "Day" : "30", "Quantity" :19 }, { "Day" : "31", "Quantity" :9 } ];
 
 // Starting point of the script execution
+define_chart_dimensions();
 generate_random_months_data();
 draw_chart_of_months(months);
+
+window.onresize = function (){
+  define_chart_dimensions();
+  draw_chart_of_months(months);
+}
 
 function draw_chart_of_months(data){
   var horizontal_scale = d3.scaleBand().domain(data.map(function(item){return item.Month})).rangeRound([0,chart_width]);
@@ -35,8 +41,16 @@ function draw_chart_of_months(data){
 
   var bar = chart.selectAll(".bar").data(data);
 
-  var g = bar.enter().append("g").attr("class", "bar");
-  g.append("rect").attr("x", function(d,i) {return horizontal_scale(d.Month) + bar_horizontal_margin})
+  var update = bar.select("rect").attr("x", function(d,i) {return horizontal_scale(d.Month) + bar_horizontal_margin})
+    .attr("y", function(d){return vertical_scale(d.Quantity)})
+    .attr("width", bar_width)
+    .on("mouseover", function(d,i){
+        d3.select(this.parentNode).append("text").attr("x", function(d,i) {return horizontal_scale(d.Month) + bar_horizontal_margin + bar_width/2 + 5}).text(d.Quantity).attr("y", function(d){return vertical_scale(d.Quantity) + 15});
+      })
+    .attr("height", function(d){ return chart_height - vertical_scale(d.Quantity)});
+
+  var enter = bar.enter().append("g").attr("class", "bar");
+  enter.append("rect").attr("x", function(d,i) {return horizontal_scale(d.Month) + bar_horizontal_margin})
     .attr("y", function(d){return vertical_scale(d.Quantity)})
     .attr("width", bar_width)
     .on("click", function(d, i) { month_selected(d)})
@@ -88,6 +102,17 @@ function draw_chart_of_days(days){
 
   var exit = bar.exit();
     exit.select("rect").transition().duration("1000").attr("height", "0");
+}
+
+function define_chart_dimensions(){
+  screen_width = get_screen_width_height().width;
+  screen_height = get_screen_width_height().height;
+
+  chart_container_width = define_chart_container_width(screen_width);
+  chart_container_height = define_chart_container_height(screen_height);
+
+  chart_width = chart_container_width - margin.right - margin.left;
+  chart_height = chart_container_height - margin.top - margin.bottom;
 }
 
 function month_selected(month){
